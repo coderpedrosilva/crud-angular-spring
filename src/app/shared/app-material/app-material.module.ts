4d133aa5be4871bdbd478900766f48c5657caedf
Pedro Silva <progerios@hotmail.com>
2022-07-31 22:00:07 -0300

    creating a service in angular

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
