ea3b2b9c4051509f09a235f99835fea006d489f7
Pedro Silva <progerios@hotmail.com>
2022-08-02 18:08:03 -0300

    list courses (HTTP GET API)

---
+++ crud-spring/src/main/java/com/spring/crudspring/controller/CourseController.java
@@ -0,0 +1,29 @@
+package com.spring.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.crudspring.model.Course;
+import com.spring.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    
+    private final CourseRepository courseRepository;
+
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+
+}
